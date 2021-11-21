49287500c67c7d5d90c5c72295910efc8b53b8bc
Ем 'Крыса' Велтер <90579260+Em-Rat@users.noreply.github.com>
2021-11-21 02:21:23 +0000

    Update clearDupes.sh

--- clearDupes.sh
+++ clearDupes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Enter file path for worklist to clean: " filePath
+read -p "Enter file path for wordlist to clean: " filePath
 read -p "Enter the file path for the new wordlist: " filePathCleaned
 
 cat $filePath | sort | uniq > $filePathCleaned
